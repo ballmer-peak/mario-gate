8a3d9dfe859deeef8e212e43358fd23409e34786
lgnirke <lgnirke@oberlin.edu>
2017-12-02 17:40:02 -0500

    launching emulator from script test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import gym
+import random
+import gym_pull
+
+env = gym.make("ppaquette/SuperMarioBros-1-1-Tiles-v0")
+observation = env.reset()
+
+print(observation)
+
+
+while(1):
+    observation, reward, done, info = env.step(env.action_space.sample())
+    print(observation)
+
+
+
